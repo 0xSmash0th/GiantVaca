0b007cba3e63d4919aee534e8b445bc2a91c4d01
Nigel Ploof <ploofn@gmail.com>
2021-02-08 08:35:11 -0500

    will now send msg when once out of the queue, i.e. it our turn

--- GIANTVACA/content.js
+++ GIANTVACA/content.js
@@ -5,6 +5,7 @@ const myKey = '#### YOUR API KEY GOES HERE'
 
 const re1 = new RegExp('There are currently no COVID-19 vaccine appointments available.', 'gi');
 const re2 = new RegExp('Our services aren\'t available right now', 'gi');
+const re3 = new RegExp('You are in line for Giant Food COVID19', 'gi');
 
 function NewTab() { 
     window.open( 
@@ -34,9 +35,10 @@ function sleep(ms) {
 async function checkPage(){
     while (true) {
         await sleep(60000)
-        const matches1 = document.documentElement.innerHTML.match(re1)
-        const matches2 = document.documentElement.innerHTML.match(re2)
-        if (matches1 || matches2) {
+        const matches1 = document.documentElement.innerHTML.match(re1) // no appts
+        const matches2 = document.documentElement.innerHTML.match(re2) // service unavailable 
+        const matches3 = document.documentElement.innerHTML.match(re3) // in the queue waiting
+        if (matches1 || matches2 || matches3) {
             window.location.href = 'https://giantfoodsched.rxtouch.com/rbssched/program/covid19'
         } else {
             textMe(phn1);
